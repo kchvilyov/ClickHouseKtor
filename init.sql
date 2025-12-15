fb64c04f310efe069ce17e2443e4b3ffdadf822b
Konstantin Chvilyov <chvilyov.k@iri.center>
2025-12-15 21:50:27 +0300

    Добавил clickhouse, создание таблицы, docker, compose.

--- init.sql
+++ init.sql
@@ -1,7 +1,10 @@
+-- Создаём базу данных, если ещё не существует
+CREATE DATABASE IF NOT EXISTS ktor_db;
 -- Создаём таблицу, если ещё не существует
-CREATE TABLE IF NOT EXISTS clicks (
+CREATE TABLE IF NOT EXISTS ktor_db.clicks (
+    id UUID DEFAULT generateUUIDv4(),
     page String,
     user_id String,
     timestamp DateTime DEFAULT now()
-) ENGINE = MergeTree()
-ORDER BY (timestamp);
+    ) ENGINE = MergeTree()
+    ORDER BY (timestamp);
